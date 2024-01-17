19f39571864ddf687bfd7fad21f2510d1d50f0b5
Mingzhe Zou <zoumingzhe@qq.com>
2024-01-17 17:58:08 +0800

    add sync scripts

---
+++ scripts/sync-from-public-dns.info.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/env bash
+
+echo "PWD: ${PWD}"
+echo "PATH: ${PATH}"
+echo "SHELL: ${SHELL}"
+
+DATADIR=$(dirname $(dirname $0))/nameservers
+echo "save all resources to dir: ${DATADIR}"
+
+wget -P ${DATADIR} --page-requisites --no-host-directories "https://public-dns.info/nameservers-all.csv"
+wget -P ${DATADIR} --page-requisites --no-host-directories "https://public-dns.info/nameservers-all.txt"
+wget -P ${DATADIR} --page-requisites --no-host-directories "https://public-dns.info/nameservers.csv"
+wget -P ${DATADIR} --page-requisites --no-host-directories "https://public-dns.info/nameservers.txt"
+
+git add ${DATADIR} && git commit --message="sync from public-dns.info $$(date '+%F %T')"
